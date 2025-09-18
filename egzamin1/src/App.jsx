f2bbae38155ef697bce04d74d580f1a8e4b6250b
marti <martynciahotuwa@gmail.com>
2025-09-18 13:34:36 +0200

    lol

---
+++ egzamin1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useRef } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
